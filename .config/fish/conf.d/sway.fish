1b6583ad4c37d07adb3576396f3debb47b5af5f3
J. Simon Richard <jsimonrichard@gmail.com>
2022-09-16 11:17:10 -0400

    update wlsunset, sway qt settings

--- .config/fish/conf.d/sway.fish
+++ .config/fish/conf.d/sway.fish
@@ -1,8 +1,9 @@
 if test (tty) = "/dev/tty1"
   set -Ux SDL_VIDEODRIVER wayland
   set -Ux _JAVA_AWT_WM_NONREPARENTING 1
-  set -Ux QT_QPA_PLATFORM wayland
+  set -Ux QT_QPA_PLATFORM "wayland;xcb"
   set -Ux XDG_CURRENT_DESKTOP sway
   set -Ux XDG_SESSION_DESKTOP sway
-  sway
+  sway || true
+  logout
 end
