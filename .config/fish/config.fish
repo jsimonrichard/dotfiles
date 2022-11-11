a063dac7ab63715dfcefb2cfcb0a717add23608e
J. Simon Richard <jsimonrichard@gmail.com>
2022-11-11 14:02:02 -0500

    Remove alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,6 @@ if status is-interactive
     fish_vi_key_bindings
 end
 
-alias vim='nvim'
 alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 abbr sps 'sudo pacman -S'
 abbr ri 'rua install'
