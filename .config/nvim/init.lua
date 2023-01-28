0bc4ece3277e8c8610ae528ad2cd7947f881debf
J. Simon Richard <jsimonrichard@gmail.com>
2023-01-28 14:33:44 -0500

    Color theme change

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -16,6 +16,10 @@ vim.cmd([[
   autocmd BufEnter * if bufname('#') =~ 'NERD_tree_\d\+' && bufname('%') !~ 'NERD_tree_\d\+' && winnr('$') > 1 |
       \ let buf=bufnr() | buffer# | execute "normal! \<C-W>w" | execute 'buffer'.buf | endif
 ]])
+
+-- Font
+vim.opt.guifont = { "Fira Code", ":h12" }
+
 -- Options
 vim.opt.mouse = "a"
 vim.opt.spell = true
