307a385bb5209b0b1f468b10c2cf6693e8871940
J. Simon Richard <jsimonrichard@gmail.com>
2023-01-29 01:57:59 -0500

    Add greeter

--- .config/fish/conf.d/sway.fish
+++
@@ -1,9 +0,0 @@
-if test (tty) = "/dev/tty1"
-  set -Ux SDL_VIDEODRIVER wayland
-  set -Ux _JAVA_AWT_WM_NONREPARENTING 1
-  set -Ux QT_QPA_PLATFORM "wayland;xcb"
-  set -Ux XDG_CURRENT_DESKTOP sway
-  set -Ux XDG_SESSION_DESKTOP sway
-  sway || true
-  logout
-end
